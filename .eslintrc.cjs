297e36dd07a81d8284bd9a4df3f04219813e4e48
Kathrine Stangeland <kathrine.kodehode@gmail.com>
2024-03-22 11:57:20 +0100

    --

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,41 +1,40 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended-type-checked',
-      'plugin:@typescript-eslint/stylistic-type-checked',
-      'plugin:react/recommended',
-      'plugin:react-hooks/recommended',
-      'plugin:import/typescript',
-      'plugin:prettier/recommended',
-      
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-      project: './tsconfig.json',
-  },
-  plugins: ['react-refresh'],
-  rules: {
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      '@typescript-eslint/consistent-type-definitions': 'off', // turning it off for now
-      'react-hooks/exhaustive-deps': 'off', // turning it off for now
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': ['error'],
-      'no-console': ['warn', { allow: ['warn', 'error', 'info', 'debug'] }],
-      '@typescript-eslint/no-misused-promises': 'off',
-      'prettier/prettier': [
-          'error',
-          {
-              endOfLine: 'auto',
-          },
-      ],
-      'no-console': ['warn', { allow: ['warn', 'error', 'info', 'debug'] }],
-  },
-  settings: {
-      react: { version: 'detect' },
-  },
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:import/typescript',
+        'plugin:prettier/recommended',
+    ],
+    ignorePatterns: ['.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: './tsconfig.json',
+    },
+    plugins: ['react-refresh'],
+    rules: {
+        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+        '@typescript-eslint/consistent-type-definitions': 'off', // turning it off for now
+        'react-hooks/exhaustive-deps': 'off', // turning it off for now
+        'react/react-in-jsx-scope': 'off',
+        'react/prop-types': 'off',
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': ['error'],
+        'no-console': ['warn', { allow: ['warn', 'error', 'info', 'debug'] }],
+        '@typescript-eslint/no-misused-promises': 'off',
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto',
+            },
+        ],
+        'no-console': ['warn', { allow: ['warn', 'error', 'info', 'debug'] }],
+    },
+    settings: {
+        react: { version: 'detect' },
+    },
 };
